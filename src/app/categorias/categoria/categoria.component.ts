097c93bd7a4a1f8df106cfe0cac719459ecf5818
Dherkyan R. Silva <dherkyan@gmail.com>
2026-03-01 10:38:52 -0300

    58. Trabalhando com Formularios Reativos (Reactive Forms)

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-categoria',
-  standalone: false,
-  templateUrl: './categoria.component.html',
-  styleUrl: './categoria.component.scss'
+    selector: 'app-categoria',
+    standalone: false,
+    templateUrl: './categoria.component.html',
+    styleUrl: './categoria.component.scss'
 })
 export class CategoriaComponent {
+
+    camposForm: FormGroup;
+
+
+    constructor() {
+        this.camposForm = new FormGroup({
+            nome: new FormControl('', Validators.required),
+            descricao: new FormControl('', Validators.required)
+        });
+    }
+
+
+    salvar() {
+        console.log('Valores: ', this.camposForm.value);
+        console.log('Valido: ', this.camposForm.valid);
+    }
 
 }
