f3debedbbd6701894e5472a759c48057a567dfcb
Dherkyan R. Silva <dherkyan@gmail.com>
2026-03-02 03:40:05 -0300

    69. Criando a estrutura da tela de lugares

--- src/app/lugares/lugares-routing.module.ts
+++ src/app/lugares/lugares-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LugarComponent} from "./lugar/lugar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: LugarComponent,
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class LugaresRoutingModule { }
+export class LugaresRoutingModule {
+}
--- src/app/lugares/lugares.module.ts
+++ src/app/lugares/lugares.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LugaresRoutingModule } from './lugares-routing.module';
 import { LugarComponent } from './lugar/lugar.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { LugarComponent } from './lugar/lugar.component';
   ],
   imports: [
     CommonModule,
-    LugaresRoutingModule
+    LugaresRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LugaresModule { }
