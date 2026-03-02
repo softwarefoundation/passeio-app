5f05dc038140048c31b29abc319345d3afb9a873
Dherkyan R. Silva <dherkyan@gmail.com>
2026-03-02 05:28:06 -0300

    75. Codificando a página de galeria com os cards dos lugares

--- src/app/galeria/galeria/galeria.component.ts
+++ src/app/galeria/galeria/galeria.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Lugar} from "../../lugares/lugar";
+import {Categoria} from "../../categorias/categoria";
+import {LugarService} from "../../lugares/lugar.service";
+import {CategoriaService} from "../../categorias/categoria.service";
 
 @Component({
-  selector: 'app-galeria',
-  standalone: false,
-  templateUrl: './galeria.component.html',
-  styleUrl: './galeria.component.scss'
+    selector: 'app-galeria',
+    standalone: false,
+    templateUrl: './galeria.component.html',
+    styleUrl: './galeria.component.scss'
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements OnInit {
+
+    lugares: Lugar[] = [];
+    categoriasFiltro: Categoria[] = [];
+
+    constructor(private lugarService: LugarService, private categoriaService: CategoriaService) {
+    }
+
+    ngOnInit(): void {
+        this.categoriaService.obterTodos().subscribe(categoriasResponse => {
+            this.categoriasFiltro = categoriasResponse;
+        });
+
+        this.lugarService.obterTodos().subscribe(lugaresResponse => {
+            this.lugares = lugaresResponse;
+        });
+    }
+
 
 }
